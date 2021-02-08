4ca57c8512c20c51f5480f753b7b2a33f22f5f3d
frmendez21 <frmendez209@gmail.com>
2021-02-08 13:34:09 -0800

    phase 0

--- W4D1/KnightTravails/knightpath.rb
+++ W4D1/KnightTravails/knightpath.rb
@@ -1,3 +1,12 @@
-Class Knight
-
+require_relative './00_tree_node.rb'
+class KnightPathFinder
+    attr_accessor :root_node
+    
+    def initialize(position)
+        @position = position
+        @root_node = nil
+    end
+    def build_move_tree 
+        self.root_node = PolyTreeNode.new(@position) if @root_node == nil
+    end
 end 
