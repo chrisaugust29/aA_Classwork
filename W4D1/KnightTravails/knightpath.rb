2f6b5b55c873afb46360efede3d560d0afe3bea9
christopherclow <christopher.ck.low@gmail.com>
2021-02-08 14:49:16 -0800

    switch

--- W4D1/KnightTravails/knightpath.rb
+++ W4D1/KnightTravails/knightpath.rb
@@ -1,12 +1,45 @@
 require_relative './00_tree_node.rb'
 class KnightPathFinder
     attr_accessor :root_node
-    
-    def initialize(position)
-        @position = position
-        @root_node = nil
+
+    #moves: [0,0]start  [1,2][
+        MOVES = [
+                [2,1],
+                [2,-1],
+                [1,2],
+                [1,-2],
+                [-1,2],
+                [-1,-2],
+                [-2,1],
+                [-2,-1]
+                ]
+
+
+    ##anything within the 8x8 board 
+    #anything outside of range of (0,0) & (7,7) is invalid 
+    #returns array of valid moves 
+
+    def self.valid_moves(position)  #any start position 
+        valid_pos = []
+        x, y = position   #as long 0 >x < 7 
+
+        MOVES.each do |sub|
+            sub_x, sub_y = sub
+            added_pos = [sub_x + x, sub_y + y]
+            if (added_pos[0] > 0 && added_pos[0] < 7) && (added_pos[1]>0 && added_pos[1]<7 ) 
+                valid_pos << added_pos
+            end
+        end
+        valid_pos
     end
-    def build_move_tree 
-        self.root_node = PolyTreeNode.new(@position) if @root_node == nil
+    
+    def initialize(start_position) 
+        @position = start_position
+        @considered_pos = [start_position]
+        @root_node = PolyTreeNode.new(@position) 
+        build_move_tree
+    end
+    def build_move_tree #all possible moves  
+        self.root_node
     end
 end 
