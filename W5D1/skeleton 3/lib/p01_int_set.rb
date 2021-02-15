6cc02fb213c254f946a33a4ca2d47c544f8dff1b
Cristian Reynoso <cristianreynoso91@gmail.com>
2021-02-15 14:59:04 -0800

    most of resizingIntSet

--- W5D1/skeleton 3/lib/p01_int_set.rb
+++ W5D1/skeleton 3/lib/p01_int_set.rb
@@ -43,17 +43,21 @@ class IntSet
   end
 
   def insert(num)
+    self[num] << num
   end
 
   def remove(num)
+    self[num].delete(num)
   end
 
-  def include?(num)
+  def include?(num=nil)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
+    @store[num % num_buckets]
     # optional but useful; return the bucket corresponding to `num`
   end
 
@@ -71,18 +75,28 @@ class ResizingIntSet
   end
 
   def insert(num)
+    if !self[num].include?(num)
+      self[num] << num
+      @count += 1
+    end
   end
 
   def remove(num)
+    if self[num].include?(num)
+      self[num].delete(num)
+      @count -= 1
+    end
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -90,5 +104,10 @@ class ResizingIntSet
   end
 
   def resize!
+    if @count == num_buckets
+      # old = @store
+      @store = Array.new(num_buckets * 2) { Array.new }
+      @count = 0
+    end
   end
 end
