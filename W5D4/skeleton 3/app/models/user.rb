9ec7170fa218bcdbb6b065a14379169a29bfdf72
christopherclow <christopher.ck.low@gmail.com>
2021-02-18 14:42:06 -0800

    Enrollment

--- W5D4/skeleton 3/app/models/user.rb
+++ W5D4/skeleton 3/app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    has_many :enrollments,
+        primary_key: :id,
+        foreign_key: :student_id, 
+        class_name: :Enrollment
+
+        #need to a through for enrolled courses 
 end
