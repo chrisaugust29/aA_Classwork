eb33faea22195cabcf90a987492c01a71b1ce894
jadin-press <jadinp@gmail.com>
2021-03-08 10:59:14 -0800

    switch

--- W8D1/reddit/app/controllers/sessions_controller.rb
+++ W8D1/reddit/app/controllers/sessions_controller.rb
@@ -1,2 +1,23 @@
 class SessionsController < ApplicationController
+  before_action :require_signed_in!
+
+  def new
+    render: new
+  end
+
+  def create
+    @user = User.self_find_by_credentials(params[:user][:username], params[:user][:password])
+    if @user
+      sign_in!(@user)
+      redirect_to subs_url
+    else
+      flash.now[:error] = ["Wrong Password"]
+      render :new
+    end
+  end
+
+  def destroy
+    sign_out!
+    redirect_to new_session_url
+  end
 end
