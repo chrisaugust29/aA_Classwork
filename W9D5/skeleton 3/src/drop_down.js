89e6d3a3a4c99a0d71513399b8930eb939b1f148
Jason Li <li.jasontse@gmail.com>
2021-03-19 15:33:47 -0700

    finish clock.js and created a / li tags for dog

--- W9D5/skeleton 3/src/drop_down.js
+++ W9D5/skeleton 3/src/drop_down.js
@@ -9,3 +9,21 @@ const dogs = {
   "French Bulldog": "https://www.akc.org/dog-breeds/french-bulldog/" 
 };
 
+const dogNav = document.getElementsByClassName('drop-down-dog-nav');
+
+function dogLinkCreator(dogs) {
+  // const dogLink = Object.values(dogs);
+  const dogName = Object.keys(dogs);
+
+  const a = document.createElement("a");
+  dogName.forEach(name => {
+    a.innerHTML = name;
+    a.href = dogs[name];
+  });
+
+  const dogLink = document.createElement("li");
+  const link = dogLink.className("dog-link");
+  dogNav.append(link);
+  // dogLink.appendChild(a)
+}
+
--- W9D5/skeleton 3/src/warmup.js
+++ W9D5/skeleton 3/src/warmup.js
@@ -1,15 +1,20 @@
-
 const partyHeader = document.getElementById('party');
 
 
 export const htmlGenerator = (string, htmlElement) => {
 
+    if (htmlElement.children) {
+        const arr = Array.from(htmlElement.children)
+        arr.forEach(el => {
+            htmlElement.removeChild(el);
+        })
+    }
+    
     const p = document.createElement("p")
-        p.innerText = string
-        htmlElement.appendChild(p)
-
-   
+    p.innerText = string
+    htmlElement.appendChild(p)   
 };
 
 htmlGenerator('Party Time.', partyHeader);
-htmlGenerator('I <3 Vanilla DOM manipulation', partyHeader)
+htmlGenerator('I <3 Vanilla DOM manipulation', partyHeader);
+
