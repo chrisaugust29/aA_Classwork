1520c90a85ee92d4c38abed48a482096a1a2b1b8
christopherclow <christopher.ck.low@gmail.com>
2021-03-19 14:05:57 -0700

    Warmup

--- W9D5/skeleton 3/src/warmup.js
+++ W9D5/skeleton 3/src/warmup.js
@@ -1,8 +1,15 @@
 
 const partyHeader = document.getElementById('party');
 
+
 export const htmlGenerator = (string, htmlElement) => {
 
+    const p = document.createElement("p")
+        p.innerText = string
+        htmlElement.appendChild(p)
+
+   
 };
 
 htmlGenerator('Party Time.', partyHeader);
+htmlGenerator('I <3 Vanilla DOM manipulation', partyHeader)
